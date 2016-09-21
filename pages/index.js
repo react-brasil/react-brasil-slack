3588af24d3ca4503c93423a4296417bd06da5f92
femontanha <femontanhanew@gmail.com>
2016-09-21 01:02:29 -0300

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { prefixLink } from 'gatsby-helpers'
+import {Container} from 'react-responsive-grid'
+import styles from './index.module.css'
+import Helmet from 'react-helmet'
+import { config } from 'config'
+import slackImage from '../assets/img/slack-react-brasil.png'
+import logoImage from '../assets/img/react-brasil.svg'
+
+export default class HomePage extends React.Component {
+  render () {
+    return (
+      <Container className={styles['container']}>
+        <Helmet title={`${config.siteTitle}`} />
+        <img src={logoImage} className={styles['site-logo']} />
+        <h1 className={styles['site-title']}>
+          React Brasil Slack
+        </h1>
+        <p>
+          Um grupo criado para discutir tudo sobre ReactJS
+        </p>
+        <img src={slackImage} className={styles['slack-image']} />
+        <a href="https://react-brasil-slack.herokuapp.com" className={styles['button']}>
+          Quero participar
+        </a>
+      </Container>
+    )
+  }
+}
+
